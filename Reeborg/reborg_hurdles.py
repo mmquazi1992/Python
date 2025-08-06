a81bc7c4811662e632c421421210c1389c748609
Mohsin Quazi <82691478+mmquazi1992@users.noreply.github.com>
2025-08-06 22:36:37 +1200

    Update reborg_hurdles.py

--- Reeborg/reborg_hurdles.py
+++ Reeborg/reborg_hurdles.py
@@ -1,27 +1,30 @@
+# function to turn right
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
 
+# function to go up
 def go_up():
     while not front_is_clear():
         turn_left()
         move()
         turn_right()
 
+# function to go down
 def go_down():
     turn_right()
     while front_is_clear():
         move()
     turn_left()
 
-    
+# function to go jump the hurdle   
 def jump_the_hurdle():
     go_up()
     move()
     go_down()
     
- 
+# function to go move through the hurdle   
 def move_carefully():
     if not front_is_clear():
         jump_the_hurdle()
