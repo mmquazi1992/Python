d7fca302f8bf666ce0a985a86711df5e61bc2e6d
Mohsin Quazi <82691478+mmquazi1992@users.noreply.github.com>
2025-08-20 22:43:37 +1200

    Update main.py

--- hangman/main.py
+++ hangman/main.py
@@ -3,30 +3,25 @@ import hangman_words
 import random
 
 word_to_guess = random.choice(hangman_words.word_list) 
-
-
-blanks = ""   
-for letters in word_to_guess:
-    blanks += "_"
 print(word_to_guess)
-print(f"Word to guess: {blanks}")
 
 
-substring_to_find ="_"
+blanks=""
+for letters in word_to_guess:
+    blanks+="_"
+print(blanks)
 
-while substring_to_find in blanks:
-    user_input = input("Guess your letter: ")
-    
-    letter_index = word_to_guess.index(user_input)
-    
-    if (type(letter_index)) == int:
-        print (f"You Guessed {user_input} which is correct. ")
-        blanks_list = list(blanks)
-        blanks_list[letter_index] = user_input
-        blanks = "".join(blanks_list)
-        print(blanks)
+
+user_input = input("Guess your letter: ").lower()
+print(user_input)
+   
+display = ""
+
+for letters in word_to_guess:
+    if user_input==letters:
+       display+=letters
+        
     else:
-        print(f"You guess {user_input} which is incorrect. You lose a life.")
+        display += "_"
 
-
-
+print(display)
